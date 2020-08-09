ff50d692521940a91ff51a182eeb81761411ebe6
hywn <jh7qbe@virginia.edu>
2020-08-09 13:12:19 -0400

    replaced paging home page with getting all videos from 'videos' tab async
    now each run of the script regenerates literally everything... including subs
    
    idk if this is good or bad? but it's equally as fast and the code is cleaner...
    
    notes:
    - for some reason 'videos' will occasionally have different video order than home page... not sure why
    - home page has extra related videos from other channels e.g. idol room

--- vsus.js
+++ vsus.js
@@ -16,20 +16,26 @@ const get_video_subs =
 	async vid_num =>
 		(await get_video_json(await get_video_info(vid_num))).captions
 
-import saving from './saving.js'
-import { update } from './um.js'
+import { writeJson } from 'https://deno.land/std/fs/write_json.ts'
+import get_videos from './hi.js'
 
 const group =
 	n => array =>
 		new Array(Math.ceil(array.length / n)).fill(0)
 			.map((_, i) => array.slice(i * n, (i + 1) * n))
 
+const wait =
+	ms =>
+		new Promise((res) => setTimeout(res, ms))
+
+
 const update_subs =
 	async channel_code =>
 {
-	await update(channel_code)
-
-	const { save, loaded: old_videos } = await saving(`${channel_code}.json`, [])
+	const old_videos = await get_videos(channel_code)
+	console.log(old_videos[0])
+	console.log('waiting...')
+	await wait(5000)
 
 	for (const grp of group(500)(old_videos))
 		await Promise.all(
@@ -43,7 +49,7 @@ const update_subs =
 				})
 		)
 
-	await save(old_videos)
+	await writeJson(`${channel_code}.json`, old_videos)
 	return old_videos
 }
 
