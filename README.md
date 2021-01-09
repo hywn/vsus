eee1a82f2723437b4ac6513d3b9112eff00c5856
James <jh7qbe@virginia.edu>
2021-01-09 13:12:17 -0500

    updated for new vlive site

--- README.md
+++ README.md
@@ -1,37 +1,18 @@
 # vsus
-get vlive subs
+javascript utility for fetching vlive video info
 
-## description
-naver vlive does not (as of aug 2020) offer any convenient way of viewing a channel's videos and available subtitles.
+most useful function is the async `json_from_url`, which given a url to vlive video gives you a lot of info including video streams and caption info
 
-vsus offers interface to naver API with technique [described here](https://gist.github.com/hywn/5a06cb56a9773407d745285c83cf3329).
-
-## usage
-`vsus.js` exports one function `update_subs(channel_code, limit=false) -> [video]`.
-- `channel_code`: the vlive 'channel code' e.g. `'CE2621'`
-- `limit`: optional, when set to a number will only fetch `limit` videos
-	- mainly for debugging/development purposes
-
-it returns array of videos, which have a lot of extra information that might be useful to you, but for main purpose of vsus, contains `subs` attribute which contains subs info.
-
-## examples
-I have misc `export_aaaa.js` scripts that all use `vsus` to export the data in some format.
-
-an example is [`./export_idle.js`](./export_idle.js); it does the following:
-- gets list of all gidle vlives and their subs from vlive
-- gets list of livestream vlives by scraping fan-generated info from gidle namu wiki page
-- exports all livestream vlives sub info in an HTML file to be comfortably viewed
+[example info returned by `json_from_url`](https://gist.github.com/hywn/634da502130eff807c3f8a9963b6069a)
 
 ## technical notes
-- each call of `update_subs` completely generates the whole video list from nothing
-	- earlier non-async versions cached which video infos you already had and would not update them, but sometimes people go back and sub old vlives so was not ideal.
-- getting the list of all videos without subs is not that expensive
-	- you can get it paged at 100+ videos in one request, so even for channel that has 1000 vids is only 10 requests
-- getting the list of all videos with subs is pretty expensive
-	- each video requires 2 requests to get subs info for (this was shortest way I could find)
-		- so for 1000 vids you have to make 2000 requests
-			- and this runs literally every time you run the script
-	- atm there is slight bug where naver will reject you or something and script exits with error. should prob fix
-	- even though expensive actually quite fast with async `fetch` (and naver's generosity)
-		- but still makes me a bit uncomfortable making 2000 requests
-		- async stuff must be grouped into big blocking calls to avoid opening too many file descriptors
+the orig/main purpose of vsus is to fetch video caption info. the current method used by vsus is to:
+1. scrape json containing relevant `vodId` and `videoSeq` off of video's page (works for both /post/ and /video/ urls)
+2. use `videoSeq` to fetch `inkey`
+3. use `vodId`, `videoSeq`, and `inkey` to fetch video json data, which includes caption info
+
+this is based on what I observed the actual service do. please let me know if anyone has any better method
+
+[outdated writeup describing old method for old vlive site](https://gist.github.com/hywn/5a06cb56a9773407d745285c83cf3329)
+
+[useful code if you want to quickly fetch a list of all a channel's videos](https://github.com/hywn/vsus/blob/544719c8add185759022d44ecf6cb0d7eee4e1d4/vsus.js#L25-L57) (this uses api that seems to not be used on site anymore? so might not work in future)
--- vsus.js
+++ vsus.js
@@ -1,104 +1,29 @@
-/******************************************************/
-/** fetching subs for single video with its videoSeq **/
-/******************************************************/
-
-// videoSeq => { master_id, key }
-const get_video_info =
-	vid_num =>
-		fetch(`https://www.vlive.tv/video/${vid_num}`)
+const info_from_url =
+	url =>
+		fetch(url)
 			.then(r => r.text())
-			.then(t => t.match(/vlive\.video\.init\(.+?\);/s)[0])
-			.then(all => [...all.matchAll(/\"(.+?)\"/g)].map(m => m[1]))
-			.then(args => ({ master_id: args[5], key: args[6] }))
+			.then(t => t.match(/(?<=window\.__PRELOADED_STATE__=).+?(?=<\/script>)/s)[0])
+			.then(t => JSON.parse(t))
 
-// { master_id, key } => { video json!!! }
-const get_video_json =
-	({ master_id, key }) =>
-		fetch(`https://apis.naver.com/rmcnmv/rmcnmv/vod/play/v2.0/${master_id}?key=${key}`)
+const inkey_from_info =
+	({ postDetail: { post: { officialVideo: { videoSeq } } } }) =>
+		// originally had an appId prob doesnt matter though
+		fetch(`https://www.vlive.tv/globalv-web/vam-web/video/v1.0/vod/${videoSeq}/inkey`, {
+			headers: { referer: `https://www.vlive.tv/video/${videoSeq}` }
+		}).then(r => r.json())
+
+const json_from_info_inkey =
+	({ postDetail: { post: { officialVideo: { vodId } } } }, { inkey }) =>
+		fetch(`https://apis.naver.com/rmcnmv/rmcnmv/vod/play/v2.0/${vodId}?key=${inkey}`)
 			.then(r => r.json())
 
-// videoSeq => { video json!!! }
-const get_video_subs =
-	async vid_num =>
-		(await get_video_json(await get_video_info(vid_num))).captions
+const json_from_url =
+	async url => {
+		const info = await info_from_url(url)
+		const inkey = await inkey_from_info(info)
+		return await json_from_info_inkey(info, inkey)
+	}
 
-/***********************************/
-/** fetching all videos (no subs) **/
-/***********************************/
+console.log(JSON.stringify(await json_from_url('https://www.vlive.tv/video/171669')))
 
-// channelCode => channelSeq
-const channel_seq =
-	code =>
-{
-	const half = Math.ceil(code.length / 2)
-	const shuf = code.substring(half, code.length) + code.substring(0, half)
-	return (parseInt(shuf, 16) + 13) / 8191
-}
-
-// channelSeq => pageSize => pageNo => [video jsons!!!]
-const get =
-	seq => page_size => page =>
-		fetch(`https://api-vfan.vlive.tv/vproxy/channelplus/getChannelVideoList?app_id=8c6cc7b45d2568fb668be6e05b6e5a3b&channelSeq=${seq}&maxNumOfRows=${page_size}&pageNo=${page}`)
-			.then(r => r.json())
-
-// channelCode => [video jsons!!!]
-const get_videos =
-	async (code, limit=false, page_size=100) =>
-{
-	const getvids = get(channel_seq(code))
-
-	const num_videos = limit || (await getvids(1)(1)).result.totalVideoCount
-
-	const page_nums = new Array(Math.ceil(num_videos / page_size)).fill(0).map((_, i) => i + 1)
-
-	return (await Promise.all(page_nums.map(getvids(page_size))))
-		.map(r => r.result.videoList)
-		.flat()
-}
-
-/**********/
-/** util **/
-/**********/
-
-// 3 => [1, 2, 3, 4, 5, 6, 7] => [[1, 2, 3], [4, 5, 6], [7]]
-const group = n => array => new Array(Math.ceil(array.length / n)).fill(0).map((_, i) => array.slice(i * n, (i + 1) * n))
-
-// resolve after millis ms
-const wait = millis => new Promise((res) => setTimeout(res, millis))
-
-/*****************/
-/** actual code **/
-/*****************/
-
-// channelCode => [video jsons augmented with .subs]
-const update_subs =
-	async (channel_code, limit=false) =>
-{
-	// fetch all videos from VLIVE
-	console.log('fetching videos...')
-	const videos = await get_videos(channel_code, limit)
-	console.log(`got ${videos.length} videos`)
-
-	// wait for a bit
-	// naver tends to be unpredictably uncooperative
-	// waiting maybe helps ??
-	if (!limit)
-		await wait(2500)
-
-	// fetch all subs from VLIVE and store in videos
-	for (const grp of group(500)(videos))
-		await Promise.all(
-			grp
-				.filter(v => !v.subs)
-				.map(async v => {
-					v.subs = (await get_video_subs(v.videoSeq)) || null
-
-					v.subs ? console.log(`got subs for ${v.videoSeq}!!`)
-					       : console.log(`${v.videoSeq} has no subs ):`)
-				})
-		)
-
-	return videos
-}
-
-export default update_subs
+export { info_from_url, inkey_from_info, json_from_info_inkey, json_from_url }
